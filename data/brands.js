19623d771f845382d59d2a4fc1d60fec901dc0c1
Nandan Singh Danu <thenandansingh1999@gmail.com>
2026-02-25 22:44:35 +0530

    brands updated

--- data/brands.js
+++ data/brands.js
@@ -1,6 +1,6 @@
 export const brands = [
   "apple.png", "sony.png", "paytm.png", "Mg.png", "infinix.png", "tecno.png" ,
-  "ucb.png","Truflo.png", "nhrd.png", "wattPower.png",'visit.png',
+  "ucb.png","Truflo.png", "nhrd.png", "wattpower.png",'visit.png',
   "innisfrer.png",  "panasonic.png", "vivo.png", 
   "JBL.png", "volvo.png","Hero.png", "levis.png"
 ];
